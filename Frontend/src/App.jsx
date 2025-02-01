d6df68bcf27ae70a1764bd16fd7b0e5f3c2b0fdb
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-02-01 10:57:10 +0530

    Adding Tailwind and vite Configuration

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <div className='text-3xl bg-red-200 font-bold underline'>Hello LMS </div>
+    </>
+  )
+}
+
+export default App
