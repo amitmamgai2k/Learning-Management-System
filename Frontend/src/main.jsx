7368530c107a8f39e06b857d9eb778ad5b0d1a4d
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-02-01 12:00:35 +0530

    Started adding basic redux

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,17 @@ import './index.css'
 import ReactDOM from 'react-dom/client'
 import {Toaster} from 'react-hot-toast'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
 import App from './App.jsx'
+import store from './Redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
 <BrowserRouter>
 
     <App />
     <Toaster/>
 
   </BrowserRouter>
+  </Provider>
 )
